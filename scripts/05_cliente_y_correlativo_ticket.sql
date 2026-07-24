e919b05934811114239aaa7fd0c535fb46153c67
javiwar04 <javier.guerra294@gmail.com>
2026-07-23 23:02:50 -0600

    Corregir script de cliente y correlativo

--- scripts/05_cliente_y_correlativo_ticket.sql
+++ scripts/05_cliente_y_correlativo_ticket.sql
@@ -2,9 +2,12 @@
 -- - Ordenes.cliente_nombre: default "Consumidor Final".
 -- - Pagos.ticket_numero/ticket_correlativo: numero bonito y estable para tickets.
 -- - Pagos.establecimiento_id: copia la sucursal de la orden para numerar por sucursal.
+--
+-- Nota tecnica:
+-- SQL Server compila cada batch antes de ejecutar ALTER TABLE. Por eso las
+-- columnas se agregan en batches separados con GO antes de referenciarlas.
 
 SET XACT_ABORT ON;
-BEGIN TRANSACTION;
 
 IF COL_LENGTH('dbo.Ordenes', 'cliente_nombre') IS NULL
 BEGIN
@@ -12,21 +15,28 @@ BEGIN
       ADD cliente_nombre nvarchar(160) NOT NULL
           CONSTRAINT DF_Ordenes_cliente_nombre DEFAULT N'Consumidor Final';
 END;
+GO
 
 IF COL_LENGTH('dbo.Pagos', 'establecimiento_id') IS NULL
 BEGIN
     ALTER TABLE dbo.Pagos ADD establecimiento_id nvarchar(36) NULL;
 END;
+GO
 
 IF COL_LENGTH('dbo.Pagos', 'ticket_numero') IS NULL
 BEGIN
     ALTER TABLE dbo.Pagos ADD ticket_numero int NULL;
 END;
+GO
 
 IF COL_LENGTH('dbo.Pagos', 'ticket_correlativo') IS NULL
 BEGIN
     ALTER TABLE dbo.Pagos ADD ticket_correlativo nvarchar(40) NULL;
 END;
+GO
+
+SET XACT_ABORT ON;
+BEGIN TRANSACTION;
 
 UPDATE p
 SET p.establecimiento_id = o.establecimiento_id
@@ -71,6 +81,7 @@ BEGIN
 END;
 
 COMMIT TRANSACTION;
+GO
 
 SELECT TOP 50
     p.ticket_correlativo,
@@ -82,3 +93,4 @@ FROM dbo.Pagos p
 JOIN dbo.Ordenes o ON o.id = p.orden_id
 LEFT JOIN dbo.Establecimientos e ON e.id = p.establecimiento_id
 ORDER BY p.registrado_en DESC;
+GO
